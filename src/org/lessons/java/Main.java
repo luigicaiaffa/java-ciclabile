8ab8d997418b9d9b745a6b11a673f5446383238e
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-12 16:43:31 +0100

    first commit

---
+++ src/org/lessons/java/Main.java
@@ -0,0 +1,11 @@
+package org.lessons.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        int[] arrayNumbers = {1, 2, 3, 4, 5};
+
+        Numbers numeri = new Numbers(arrayNumbers);
+    }
+}
---
+++ src/org/lessons/java/Numbers.java
@@ -0,0 +1,21 @@
+package org.lessons.java;
+
+public class Numbers {
+
+    // # Variabili d'istanza
+    private int[] integerNumbers;
+
+    // # Costruttori
+    public Numbers(int[] numbersArray) {
+        this.integerNumbers = numbersArray;
+    }
+
+    // public int getNextElement() {
+    // prossimo elemento rispetto all'ultima volta in cui è stato invocato
+    // }
+
+    // public boolean hasNextElements() {
+    // restituisce true se ci sono ancora elementi nell'array
+    // }
+
+}
