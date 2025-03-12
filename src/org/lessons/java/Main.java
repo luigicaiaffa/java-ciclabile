2d67835f7c08f1b2520a9aee1b99075e47383953
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-12 18:43:53 +0100

    Add Bonus

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -4,25 +4,29 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] arrayNumbers = { 10, 25, 47, 78, 120 };
+        // int[] arrayNumbers = { 10, 25, 47, 78, 120 };
+        // Numbers integers = new Numbers(arrayNumbers);
+        Numbers integers = new Numbers();
 
-        Numbers integers = new Numbers(arrayNumbers);
+        integers.addElement(36);
+        integers.addElement(64);
+        integers.addElement(82);
+
         System.out.println("-------");
+
         integers.getNumbers();
 
-        System.out.println("\n");
+        // System.out.println(integers.getNextElement());
 
-        System.out.println(integers.getNextElement());
+        // System.out.println(integers.getNextElement());
+        // System.out.println(integers.hasNextElements());
 
-        System.out.println(integers.getNextElement());
-        System.out.println(integers.hasNextElements());
+        // System.out.println(integers.getNextElement());
 
-        System.out.println(integers.getNextElement());
+        // System.out.println(integers.getNextElement());
 
-        System.out.println(integers.getNextElement());
-
-        System.out.println(integers.getNextElement());
-        System.out.println(integers.hasNextElements());
+        // System.out.println(integers.getNextElement());
+        // System.out.println(integers.hasNextElements());
 
         System.out.println("-------");
 
--- src/org/lessons/java/Numbers.java
+++ src/org/lessons/java/Numbers.java
@@ -15,6 +15,11 @@ public class Numbers {
         this.currentIndex = 0;
     }
 
+    public Numbers() {
+        this.integerNumbers = new int[0];
+        this.currentIndex = 0;
+    }
+
     // # Metodi
     public void getNumbers() {
         ArrayList<Integer> integerArray = new ArrayList<>();
@@ -23,12 +28,13 @@ public class Numbers {
             integerArray.add(num);
         }
 
-        System.out.println("\n" + integerArray);
+        System.out.println("\n" + integerArray + "\n");
     }
 
     public int getNextElement() {
         int currentNumber = integerNumbers[currentIndex];
         currentIndex++;
+
         return currentNumber;
     }
 
@@ -44,4 +50,17 @@ public class Numbers {
         return iterator.hasNext();
     }
 
+    public void addElement(int newNumber) {
+        // Creo un nuovo array più grande di 1 rispetto al mio array di interi
+        int[] newArray = new int[integerNumbers.length + 1];
+
+        // Copio il mio array di interi all'interno del nuovo array
+        System.arraycopy(integerNumbers, 0, newArray, 0, integerNumbers.length);
+
+        // All'ultima posizione aggiungo il nuovo numero da inserire nell'array
+        newArray[newArray.length - 1] = newNumber;
+
+        // Setto il nuovo array come array principale
+        this.integerNumbers = newArray;
+    }
 }
